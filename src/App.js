ab0a6e1c2df5542a0b5066e4e5351817074204d1
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-12-04 12:29:24 -0300

    IF-ELSE inline c/ operdador condicional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import './App.css';
 
-
+// variaveis de elementos
 const a = <button>Historico</button>
-const b = <button></button>
-const hasCustumer = true
+const b = <button>Cadastrar</button>
+const hasCustumer = false
 
-function App() {
-  return (
-    <>
-      <h1>Mostrar ou não usando if inline com operdador logico &&</h1>
-      {hasCustumer && (
-        <div>
-          <h1>Clique para ver historico</h1>
-          {a}
-        </div>
-      )}
-    </>
-  );
+const App = () => {
+  const renderShowHistory = () => (
+    <div>
+      <h1>Clique para ver historico</h1>
+      {b}
+    </div>
+  )
+
+  const renderAddCustumer = () => (
+    <div>
+      <h1>Clique abaixo para cadastrar clientes</h1>
+      {b}
+    </div>
+  )
+
+    return (
+      <>
+        <h1>IF-ELSE inline c/ operdador condicional</h1>
+
+        {hasCustumer ? renderShowHistory() : renderAddCustumer()}
+      </>
+    )
+
 }
 
 export default App;
